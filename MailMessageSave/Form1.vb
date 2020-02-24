0f2637a947b7ca3289906a401907b1c6cc6e656a
Marc <marcalfaro@live.com>
2020-02-24 23:42:19 +0800

    Add project files.

---
+++ MailMessageSave/Form1.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Net.Mail
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Using _testMail As MailMessage = New MailMessage
+            With _testMail
+                .Body = "This is a test email"
+                .To.Add(New MailAddress("email@domain.com"))
+                .From = New MailAddress("sender@domain.com")
+                .Subject = "Test email"
+                '            .Save($"{IO.Path.Combine(Application.StartupPath, "test.eml")}")
+
+                Dim tmpFolder As String = IO.Path.Combine(Application.StartupPath, System.Guid.NewGuid.ToString)
+                If Not IO.Directory.Exists(tmpFolder) Then
+                    IO.Directory.CreateDirectory(tmpFolder)
+                End If
+
+                Using client As SmtpClient = New SmtpClient("mysmtphost")
+                    'client.UseDefaultCredentials = true;
+                    client.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory
+                    client.PickupDirectoryLocation = tmpFolder
+                    client.Send(_testMail)
+                End Using
+
+                Dim filePath As String = IO.Directory.GetFiles(tmpFolder).Single
+                MsgBox(filePath)
+            End With
+        End Using
+
+
+
+    End Sub
+End Class
---
+++ MailMessageSave/MailMessageExt.vb
@@ -0,0 +1,22 @@
+﻿Imports System.IO
+Imports System.Net.Mail
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+
+Module MailMessageExt
+    <Extension()>
+    Sub Save(ByVal Message As MailMessage, ByVal FileName As String)
+        Dim assembly As Assembly = GetType(SmtpClient).Assembly
+        Dim _mailWriterType As Type = assembly.[GetType]("System.Net.Mail.MailWriter")
+
+        Using _fileStream As FileStream = New FileStream(FileName, FileMode.Create)
+            Dim _mailWriterContructor As ConstructorInfo = _mailWriterType.GetConstructor(BindingFlags.Instance Or BindingFlags.NonPublic, Nothing, New Type() {GetType(Stream)}, Nothing)
+            Dim _mailWriter As Object = _mailWriterContructor.Invoke(New Object() {_fileStream})
+            Dim _sendMethod As MethodInfo = GetType(MailMessage).GetMethod("Send", BindingFlags.Instance Or BindingFlags.NonPublic)
+            _sendMethod.Invoke(Message, BindingFlags.Instance Or BindingFlags.NonPublic, Nothing, New Object() {_mailWriter, True}, Nothing)
+            Dim _closeMethod As MethodInfo = _mailWriter.[GetType]().GetMethod("Close", BindingFlags.Instance Or BindingFlags.NonPublic)
+            _closeMethod.Invoke(_mailWriter, BindingFlags.Instance Or BindingFlags.NonPublic, Nothing, New Object() {}, Nothing)
+        End Using
+
+    End Sub
+End Module
